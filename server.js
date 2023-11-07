8b79a0db75dbec9533b82d28bd526c76d66da1a4
thereIsNoSpoon8891 <130624304+thereIsNoSpoon8891@users.noreply.github.com>
2023-11-06 19:36:40 -0700

    update API to serve up the page

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
-require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+const app = express();
+require('dotenv').config();
+const path = require("path");
 
-
-app.use(cors())
-app.use(express.json())
-app.use("/api", require("./routes/mailRouter"))
-
+app.use(cors());
+app.use(express.json());
+app.use("/api", require("./routes/mailRouter"));
+app.use(express.static(path.join(__dirname, "client", "build")));
 
 
 
@@ -18,6 +18,9 @@ app.use((err, req, res, next) => {
     return res.send({errorMessage: err.message })
 })
 
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(8080, () => {
     console.log("server live on port 8080");
