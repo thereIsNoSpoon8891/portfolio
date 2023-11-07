5279ec2fefbd706813e5a335ecea4770ccf390f1
thereIsNoSpoon8891 <130624304+thereIsNoSpoon8891@users.noreply.github.com>
2023-11-07 14:36:18 -0700

    adjust server.js to serve up client

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const path = require("path");
 app.use(cors());
 app.use(express.json());
 app.use("/api", require("./routes/mailRouter"));
-app.use(express.static(path.join(__dirname, "client", "build")));
+app.use(express.static(path.join(__dirname, "client")));
 
 
 
@@ -19,7 +19,7 @@ app.use((err, req, res, next) => {
 })
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "client", "index.html"));
 });
 
 app.listen(8080, () => {
